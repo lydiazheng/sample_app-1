fda8f18b13c22b6f13aa0f44f1624f0c4736cbd1
郑昊 <zhenghao@d207-023-187-167.wireless.sfu.ca>
2016-10-09 15:25:26 -0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # -- all .rb files in that directory are automatically loaded.
   end
 end
